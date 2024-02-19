15cebc0829123fa5b5a91098bed3b3b1281f7a7c
yeinryu <111036037+yeinryu@users.noreply.github.com>
2024-02-18 22:54:17 -0600

    Create skating_star.js
    First, create a sketch that draws a star. Animate the star so that is slides around the canvas and bounces off the edges. Take care that the star never appears to overlap the edge of the canvas.

---
+++ skating_star.js
@@ -0,0 +1,40 @@
+let x = 320;
+let y = 180;
+let xspeed = 5;
+let yspeed = 2;
+
+let r = 25;
+
+function setup() {
+  createCanvas(640, 360);
+}
+
+function draw() {
+  background(0);
+  star(x, y, 30, 70, 5);
+  x += xspeed;
+  y += yspeed;
+  if (x > width - r || x < r) {
+    xspeed = -xspeed;
+  }
+  if (y > height - r || y < r) {
+    yspeed = -yspeed;
+  }
+
+}
+ function star(x, y, radius1, radius2, npoints) {
+  let angle = TWO_PI / npoints;
+  let halfAngle = angle / 2.0;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius2;
+    let sy = y + sin(a) * radius2;
+    vertex(sx, sy);
+    sx = x + cos(a + halfAngle) * radius1;
+    sy = y + sin(a + halfAngle) * radius1;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+
+
+}
